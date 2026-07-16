ac6d891de75557b6da389b02e40e3c32b7712926
drixouuk <drixou.uk@gmail.com>
2026-07-16 13:05:59 +0200

    migration : schedual 2

--- apps/cms/src/migrations/index.ts
+++ apps/cms/src/migrations/index.ts
@@ -1,9 +1,15 @@
-import * as migration_20260710_064021 from './20260710_064021';
+import * as migration_20260710_064021 from "./20260710_064021";
+import * as migration_20260716_fix_schedules_locales from "./20260716_fix_schedules_locales";
 
 export const migrations = [
   {
     up: migration_20260710_064021.up,
     down: migration_20260710_064021.down,
-    name: '20260710_064021'
+    name: "20260710_064021",
+  },
+  {
+    up: migration_20260716_fix_schedules_locales.up,
+    down: migration_20260716_fix_schedules_locales.down,
+    name: "20260716_fix_schedules_locales",
   },
 ];
